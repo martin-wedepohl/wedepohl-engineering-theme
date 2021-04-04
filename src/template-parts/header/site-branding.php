a423bc372622476fd4198b125060d53802a09799
Martin Wedepohl <martin.wedepohl@shaw.ca>
2021-04-04 16:47:34 -0700

    Forgot file in commit

--- src/template-parts/header/site-branding.php
+++ src/template-parts/header/site-branding.php
@@ -14,29 +14,30 @@ $header_class = $show_title ? 'site-title' : 'screen-reader-text';
 
 ?>
 
-<?php if ( has_custom_logo() && $show_title ) : ?>
-	<div class="site-logo"><?php the_custom_logo(); ?></div>
-<?php endif; ?>
-
 <div class="site-branding">
 
-	<?php if ( has_custom_logo() && ! $show_title ) : ?>
-		<div class="site-logo"><?php the_custom_logo(); ?></div>
-	<?php endif; ?>
+	<?php
+	$img_file = get_stylesheet_directory() . '/dist/img/favicon.svg';
+	if ( file_exists( $img_file ) ) {
+		echo file_get_contents( $img_file );		 // phpcs:ignore WordPress.Security.EscapeOutput
+	}
+	?>
 
-	<?php if ( $blog_info ) : ?>
-		<?php if ( is_front_page() && ! is_paged() ) : ?>
-			<h1 class="<?php echo esc_attr( $header_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1>
-		<?php elseif ( is_front_page() || is_home() ) : ?>
-			<h1 class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></h1>
-		<?php else : ?>
-			<p class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></p>
+	<div class="site-logo">
+		<?php if ( $blog_info ) : ?>
+			<?php if ( is_front_page() && ! is_paged() ) : ?>
+				<h1 class="<?php echo esc_attr( $header_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1>
+			<?php elseif ( is_front_page() || is_home() ) : ?>
+				<h1 class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></h1>
+			<?php else : ?>
+				<p class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></p>
+			<?php endif; ?>
 		<?php endif; ?>
-	<?php endif; ?>
+		<?php if ( $description && get_theme_mod( 'display_title_and_tagline', true ) === true ) : ?>
+			<p class="site-description">
+				<?php echo $description; // phpcs:ignore WordPress.Security.EscapeOutput ?>
+			</p>
+		<?php endif; ?>
+	</div> <!-- .site-logo -->
 
-	<?php if ( $description && get_theme_mod( 'display_title_and_tagline', true ) === true ) : ?>
-		<p class="site-description">
-			<?php echo $description; // phpcs:ignore WordPress.Security.EscapeOutput ?>
-		</p>
-	<?php endif; ?>
 </div><!-- .site-branding -->
